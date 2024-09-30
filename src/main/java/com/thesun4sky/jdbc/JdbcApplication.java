529732218a491c65339cc25907ee409b363cd26b
kang-sumin <min001117@gmail.com>
2024-09-30 17:38:09 +0900

    [REFACTOR] data 수정 & add comment

--- src/main/java/com/thesun4sky/jdbc/JdbcApplication.java
+++ src/main/java/com/thesun4sky/jdbc/JdbcApplication.java
@@ -19,29 +19,33 @@ public class JdbcApplication {
 		String url = "jdbc:h2:mem:test"; 	// spring.datasource.url
 		String username = "sa";				// spring.datasource.username
 
+		// connection 얻어오기
+		// try-with-resource : try문 안에서 사용할 자원을 괄호안에 선언해 주면서 try문이 종료되면 자동으로 자원 해제가 됨
 		try (Connection connection = DriverManager.getConnection(url, username, null)) {
 			try {
-				// 테이블 생성
+				// 테이블 생성 (statement 생성)
 				String creatSql = "CREATE TABLE USERS (id SERIAL, username varchar(255))";
 				try (PreparedStatement statement = connection.prepareStatement(creatSql)) {
 					statement.execute();
 				}
 
-				// 데이터 추가
-				String insertSql = "INSERT INTO USERS (username) VALUES ('teasun kim')";
+				// 데이터 추가 (statement 생성)
+				String insertSql = "INSERT INTO USERS (username) VALUES ('sumin kang')";
 				try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
-					statement.execute();
-				}
-
-				// 데이터 조회
-				String selectSql = "SELECT * FROM USERS";
-				try (PreparedStatement statement = connection.prepareStatement(selectSql)) {
-					var rs = statement.executeQuery();
-					while (rs.next()) {
-						System.out.printf("%d, %s", rs.getInt("id"), rs.getString("username"));
+					for(int i=0; i<10; i++) {
+						statement.execute();
 					}
 				}
-			} catch (SQLException e) {
+
+				// 데이터 조회 (statement 생성 후 rs = resultSet 수신 & next() 조회)
+				String selectSql = "SELECT * FROM USERS";
+				try (PreparedStatement statement = connection.prepareStatement(selectSql)) {
+					var rs = statement.executeQuery(); // 쿼리 결과를 rs에 저장함
+					while (rs.next()) { // 결과값을 하나씩 받아서 출력해 줌
+						System.out.printf("%d, %s\n", rs.getInt("id"), rs.getString("username"));
+					}
+				}
+			} catch (SQLException e) { // SQL 예외 처리
 				if (e.getMessage().equals("ERROR: relation \"account\" already exists")) {
 					System.out.println("USERS 테이블이 이미 존재합니다.");
 				} else {
